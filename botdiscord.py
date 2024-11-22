14262ebc52402b07cf40c72880bcade31ae5b236
sceva3 <riccardovigano21@gmail.com>
2024-11-22 11:31:29 +0100

    configurata variabile ambiente e token per bot con sblocco degli intents

---
+++ botdiscord.py
@@ -0,0 +1,12 @@
+import hikari
+import os
+from dotenv import load_dotenv
+
+# apro il json
+
+# carichiamo il .env file e otteniamo il token
+load_dotenv()
+
+token = os.getenv('TOKEN')
+
+bot = hikari.GatewayBot(token,intents=hikari.Intents.ALL_UNPRIVILEGED | hikari.Intents.MESSAGE_CONTENT )
